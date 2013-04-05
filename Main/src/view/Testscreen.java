065190db4dee6a26532e747deb213ac54b6db1bd
Leo <lzulfiu@tocco.ch>
2013-04-05 12:21:05 +0200

    add textbutton to testscreen

--- Main/src/view/Testscreen.java
+++ Main/src/view/Testscreen.java
@@ -3,7 +3,14 @@ package view;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import utils.Assets;
 import utils.Input;
 import utils.MyScreen;
@@ -15,13 +22,17 @@ import utils.Pluvia;
 public class Testscreen extends MyScreen {
     ShapeRenderer shapeRenderer;
     float opaque;
-    boolean fadeIn;
-    boolean fadeOut;
+    private Stage stage;
+    BitmapFont font;
 
     public Testscreen(Pluvia pluvia) {
         super(pluvia);
         shapeRenderer = new ShapeRenderer();
         opaque = 1;
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        font = new BitmapFont(Gdx.files.internal("gui/arial-15.fnt"),
+                Gdx.files.internal("gui/arial-15.png"), false);
     }
 
     @Override
@@ -34,51 +45,56 @@ public class Testscreen extends MyScreen {
         Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
         shapeRenderer.begin(ShapeRenderer.ShapeType.FilledRectangle);
         shapeRenderer.setColor(0, 0, 0, opaque);
-        shapeRenderer.filledRect(0, 0, 800, 480);
+        shapeRenderer.filledRect(0, 0, 50, 50);
         shapeRenderer.end();
         opaque-=0.01;
 
-        Gdx.gl.glEnable(GL10.GL_BLEND);
-        Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.FilledCircle);
-        shapeRenderer.setColor(1, 0, 0, opaque); // Color is between 0 and 1
-        //shapeRenderer.filledCircle(500, 200, 80); // 0 is not transparent
-        shapeRenderer.end();
-        Gdx.gl.glDisable(GL10.GL_BLEND);
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void show() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void hide() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void pause() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void resume() {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public void dispose() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        stage.draw();
     }
 
     @Override
     public void init() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        TextButton.TextButtonStyle asdf;
+        asdf = new TextButton.TextButtonStyle();
+        asdf.font = font;
+        TextButton thatButton = new TextButton("Le Button", asdf);
+        thatButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("yooouuuuu...you just pressed thaButton");
+            }
+            /* This is also possible if you need these methods
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("just touchedUP that button");
+            }
+
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("touched down that button");
+                return true;
+            }
+            */
+        });
+        thatButton.setX(300);
+        thatButton.setY(200);
+        stage.addActor(thatButton);
     }
+
+    @Override
+    public void resize(int width, int height) { }
+
+    @Override
+    public void show() { }
+
+    @Override
+    public void hide() { }
+
+    @Override
+    public void pause() { }
+
+    @Override
+    public void resume() { }
+
+    @Override
+    public void dispose() {  }
 }
